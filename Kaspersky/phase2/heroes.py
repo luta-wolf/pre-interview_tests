c2233cc367237e3bf7454b7617803847947683fe
luta-wolf <denis.skrynnikov.job@gmail.com>
2023-06-12 20:20:16 +0300

    correct solution for heroes.py

--- Kaspersky/phase2/heroes.py
+++ Kaspersky/phase2/heroes.py
@@ -1,16 +1,37 @@
-def check_mission(heroes, mission) -> tuple:
-	ability_list = []
+# Решение валится на тестах 7 и 8
+# def check_mission(heroes, mission) -> tuple:
+# 	ability_list = []
+# 	hero_list = []
+# 	for hero in heroes:
+# 		hero_list.append(hero)
+# 		ability_list.extend(hero[1])
+
+# 	for stage in mission:
+# 		if stage not in ability_list or mission.count(stage) > ability_list.count(stage):
+# 			return ()
+
+# 	hero_list.sort(key=lambda x: x[1])
+# 	return tuple([i[0] for i in hero_list])
+
+import itertools
+
+def check_mission(heroes, mission):
 	hero_list = []
-	for hero in heroes:
-		hero_list.append(hero)
-		ability_list.extend(hero[1])
-
 	for stage in mission:
-		if stage not in ability_list or mission.count(stage) > ability_list.count(stage):
-			return ()
+		available_hero = []
+		for hero in heroes:
+			if stage in hero[1]:
+				available_hero.append(hero[0])
+		if len(available_hero):
+			hero_list.append(tuple(available_hero))
+		else:
+			return tuple([])
 
-	hero_list.sort(key=lambda x: x[1])
-	return tuple([i[0] for i in hero_list])
+	combinations = list(itertools.product(*hero_list))
+	for combination in combinations:
+		if len(set(combination)) == len(mission):
+			return(combination)
+	return tuple([])
 
 
 heroes = (("Илья М.", (1, 2, 3)),)
@@ -31,10 +52,10 @@ mission5 = (1, 1, 2)
 heroes6 = (("Илья М.", (1, 2, 3)), ("Алёша П.", (1, )),)
 mission6 = (1, 5)
 
-heroes7 = (("Добрыня Н.", (1, 2, 3, 4)), ("Илья М.", (1, 2, 4)), ("Алёша П.", (1, 2, 4)), ("----- П.", (1, 2, 4)))
+heroes7 = (("Добрыня Н.", (1, 2, 3, 4)), ("Илья М.", (1, 2, 4)), ("Алёша П.", (1, 2, 4)), ("Змей Г.", (1, 2, 4)))
 mission7 = (1, 2, 3, 4)
 
-heroes8 = (("Добрыня Н.", (1, 2, 3, 4)), ("Илья М.", (1, 2)), ("Алёша П.", (2, 4)), ("----- П.", (1, 4)))
+heroes8 = (("Добрыня Н.", (1, 2, 3, 4)), ("Илья М.", (1, 2)), ("Алёша П.", (2, 4)), ("Змей Г.", (1, 4)))
 mission8 = (1, 2, 3, 4)
 
 # test Ok
@@ -44,6 +65,5 @@ print(check_mission(heroes3, mission3))
 print(check_mission(heroes4, mission4))
 print(check_mission(heroes5, mission5))
 print(check_mission(heroes6, mission6))
-# test Fail
 print(check_mission(heroes7, mission7))
 print(check_mission(heroes8, mission8))
